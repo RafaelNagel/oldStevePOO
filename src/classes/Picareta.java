bc985290fdca7f4c0ffd72a4c5ed64f8a748a51a
Rafael Nagel <rafaelnageldesouza@gmail.com>
2026-09-08 17:14:03 -0300

    fix: remove getter e setter desnecessários

--- src/classes/Picareta.java
+++ src/classes/Picareta.java
@@ -5,30 +5,6 @@ public class Picareta {
     private int durabilidade;
     private int forca;
 
-    public String getMaterial() {
-        return material;
-    }
-
-    public void setMaterial(String material) {
-        this.material = material;
-    }
-
-    public int getDurabilidade() {
-        return durabilidade;
-    }
-
-    public void setDurabilidade(int durabilidade) {
-        this.durabilidade = durabilidade;
-    }
-
-    public int getForca() {
-        return forca;
-    }
-
-    public void setForca(int forca) {
-        this.forca = forca;
-    }
-
     public Picareta(String material, int durabilidade, int forca) {
         this.material = material;
         this.durabilidade = durabilidade;
